b47d1be7ea17d471ce3057819ce10a149ad41a4d
DruxAMB <druxambmarketing@gmail.com>
2023-11-17 09:12:37 -0800

    first commit message

---
+++ README.md
@@ -0,0 +1,71 @@
+# Frontend Mentor - Tip calculator app solution
+
+This is a solution to the [Tip calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Calculate the correct tip and total cost of the bill per person
+
+### Screenshot
+
+![](./screenshot.jpg)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+
+**Note: These are just examples. Delete this note and replace the list above with your own choices**
+
+### What I learned
+
+This Challenge helped me with if statements in javaScript.
+
+### Continued development
+
+I want to focus more on forEach method() in javaScript.
+
+### Useful resources
+
+- [w3Schools](https://www.w3schools.com/jsref/jsref_foreach.asp) - This helped me with forEach method() in javaScript.
+
+## Author
+
+- Website - [Muhammad Amoo](https://druxamb.github.io/DruxAMB-Portfolio/)
+- Frontend Mentor - [@yourusername](https://www.frontendmentor.io/profile/DruxAMB)
+- Twitter - [@yourusername](https://www.twitter.com/DruxAMB)
+- Instagram - [@yourusername](https://www.instagram.com/druxamb)
+
+## Acknowledgments
+
+@michojekunle frontend mentor helped me a bit.
---
+++ main.js
@@ -0,0 +1,70 @@
+const inputs = document.querySelectorAll('.input');
+const warning = document.getElementById('warning-text');
+const tips = document.querySelectorAll('.tip');
+const totalAmountEl = document.getElementById('total-amount');
+const tipAmountEl = document.getElementById('tip-amount');
+const resetButton = document.getElementById('reset-button')
+
+inputs.forEach((input, index) => {
+    input.addEventListener('change', () => {
+        bill = input.children[0].value;
+        people = input.children[1].value;
+        console.log(input.children[1].value);
+        if(input.children[1].value <= 0){
+            if(index === 1) {
+                warning.innerText = "Can't be zero";
+            }
+            input.style.border = '1px solid red';
+        } else {
+            input.style.border = '1px solid green';
+            warning.innerText = '';
+        }
+        calculateTip()
+    })
+})
+
+tips.forEach((tip, index) => {
+    tip.addEventListener('click', () => {
+        tips.forEach(tip => tip.classList.remove('selected-tip'))
+            tip.classList.toggle('selected-tip');
+        calculateTip()
+    })
+})
+
+
+function calculateTip() {
+    let bill = document.querySelector('.bill .input input').value;
+    let people = document.querySelector('.people .input input').value;
+    let tipval; 
+    tips.forEach((tip, index) => {
+        if(tip.classList.contains('selected-tip')){
+            tipval = parseInt(tip.innerText);
+            if(index === tips.length-1){
+                tipval = parseInt(tip.value);
+            }
+        }
+    })
+
+    let tipAmount, totalAmount;
+
+    bill = parseFloat(bill);
+    people = parseFloat(people);
+
+    tipAmount = ((bill* tipval/100)/people).toFixed(2);
+    totalAmount = ((bill + (bill* tipval/100))/people).toFixed(2);
+
+    tipAmountEl.innerText = `$${tipAmount === 'NaN' ? 0.00 : tipAmount}`;
+    totalAmountEl.innerText = `$${totalAmount === 'NaN' ? 0.00 : totalAmount}`;
+}
+
+resetButton.addEventListener('click', () => {
+    tips.forEach(tip => tip.classList.remove('selected-tip'));
+    tips[2].classList.add('selected-tip');
+    document.querySelector('.bill .input input').value = 0.00;
+    document.querySelector('.people .input input').value = 1;
+    
+    calculateTip();
+})
+
+calculateTip();
+
